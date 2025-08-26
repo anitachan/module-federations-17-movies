4a4bae81d8a22a3758ef302d4c3d9cced784c458
anitachan <anita_chavez@outlook.com>
2025-08-26 18:45:33 -0500

    update: update angular material

--- projects/host/src/app/app.config.ts
+++ projects/host/src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GetMoviesGateway, SharedLibModule } from 'shared-lib';
 import { routes } from './app.routes';
@@ -11,7 +10,6 @@ import { CustomGetMoviesService } from './infrastructure/driven-adapter/custom-g
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(),
     provideHttpClient(withFetch()),
     importProvidersFrom(
       BrowserAnimationsModule,
